5b2543f86b10a1d6a53455a121cfc2b35e941c22
Ismael Cerezo <i.cerezo@actongroup.com>
2025-05-23 10:13:46 +0200

    feat: ajout d'une nouvelle page de configuration utilisateur pour la sélection du thème et mise à jour du composant d'en-tête avec un lien vers cette page

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,5 +1,5 @@
 import { TitleCasePipe } from "@angular/common";
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 import { MatIconModule } from "@angular/material/icon";
 import { MatMenuModule } from "@angular/material/menu";
 import { MatToolbarModule } from "@angular/material/toolbar";
@@ -7,6 +7,8 @@ import { ThemeService } from "../../service/theme.service";
 import { MatButtonModule } from "@angular/material/button";
 import { TranslateModule } from "@ngx-translate/core";
 import { LanguageService } from "../../service/language.service";
+import { MatDividerModule } from "@angular/material/divider";
+import { RouterModule } from "@angular/router";
 
 @Component({
    selector: "app-header",
@@ -18,12 +20,18 @@ import { LanguageService } from "../../service/language.service";
       TitleCasePipe,
       MatButtonModule,
       TranslateModule,
+      MatDividerModule,
+      RouterModule,
    ],
    templateUrl: "./header.component.html",
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
    constructor(
       public _themeService: ThemeService,
       public _languageService: LanguageService
    ) {}
+
+   ngOnInit(): void {
+      // This method is empty as per the original code
+   }
 }
