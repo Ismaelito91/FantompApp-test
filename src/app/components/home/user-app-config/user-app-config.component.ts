a2dee392c4b057cac48e1870edf288228811c484
Ismael Cerezo <i.cerezo@actongroup.com>
2025-05-27 11:50:03 +0200

    Refactorisation (styles) : mise à jour des déclarations de polices et des mixins de thème Déclarations de polices : Ajustement des déclarations @font-face pour les polices Lexend et Nunito afin d'améliorer la lisibilité en formatant les URLs des sources. Organisation des styles : Ajout de la directive @forward dans styles.scss pour mieux organiser les styles des thèmes. Mixins de thème : Modification des mixins theme-aware-background pour utiliser les variables de contenu au lieu des variables de fond pour le mode clair.

--- src/app/components/home/user-app-config/user-app-config.component.ts
+++ src/app/components/home/user-app-config/user-app-config.component.ts
@@ -1,10 +1,11 @@
-import { Component } from "@angular/core";
+import { Component, inject, OnInit } from "@angular/core";
 import { ThemeService, ThemeType } from "../../../service/theme.service"; // adapte le chemin si besoin
 import { MatRadioModule } from "@angular/material/radio";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
-import { RouterModule } from "@angular/router";
+import { RouterLink } from "@angular/router";
 import { NgFor, NgIf, NgSwitch, NgSwitchCase } from "@angular/common";
+import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
    selector: "app-user-app-config",
@@ -15,15 +16,33 @@ import { NgFor, NgIf, NgSwitch, NgSwitchCase } from "@angular/common";
       MatRadioModule,
       MatIconModule,
       MatButtonModule,
-      RouterModule,
+      RouterLink,
       NgFor,
       NgIf,
       NgSwitch,
       NgSwitchCase,
+      TranslateModule,
    ],
 })
-export class UserAppConfigComponent {
+export class UserAppConfigComponent implements OnInit {
+   private translateService = inject(TranslateService);
+
    constructor(public themeService: ThemeService) {}
+
+   async ngOnInit() {
+      try {
+         // Initialisation des traductions
+         await this.translateService.use("fr").toPromise();
+
+         // Vérification que les traductions sont chargées
+         const translation = await this.translateService
+            .get("USER_CONFIG_APP.TITLE")
+            .toPromise();
+         console.log("Translation loaded:", translation);
+      } catch (error) {
+         console.error("Error loading translations:", error);
+      }
+   }
 
    get themes() {
       return this.themeService.getThemes();
