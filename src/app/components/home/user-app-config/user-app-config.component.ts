12608eac860aa25213091e33c50a8ed45c3d490a
Ismael Cerezo <i.cerezo@actongroup.com>
2025-05-29 17:21:01 +0200

    feat(user-app-config): amélioration de l'accessibilité et de la sélection de thème
    - Mise à jour des aria-labels et titles pour une meilleure accessibilité en utilisant les clés de traduction.
    - Remplacement des titres par des divs pour améliorer la structure sémantique.
    - Simplification de la logique de sélection de thème en supprimant les ngFor et ngSwitch inutiles.
    - Ajout de texte visuellement masqué pour les lecteurs d'écran afin d'améliorer l'utilisabilité.
    - Introduction de nouveaux styles pour la classe sr-only pour masquer visuellement les éléments tout en les gardant accessibles.

--- src/app/components/home/user-app-config/user-app-config.component.ts
+++ src/app/components/home/user-app-config/user-app-config.component.ts
@@ -1,10 +1,9 @@
 import { Component, inject, OnInit } from "@angular/core";
-import { ThemeService, ThemeType } from "../../../service/theme.service"; // adapte le chemin si besoin
+import { ThemeService, ThemeType } from "../../../service/theme.service";
 import { MatRadioModule } from "@angular/material/radio";
 import { MatIconModule } from "@angular/material/icon";
 import { MatButtonModule } from "@angular/material/button";
 import { RouterLink } from "@angular/router";
-import { NgFor, NgIf, NgSwitch, NgSwitchCase } from "@angular/common";
 import { TranslateModule, TranslateService } from "@ngx-translate/core";
 
 @Component({
@@ -17,10 +16,6 @@ import { TranslateModule, TranslateService } from "@ngx-translate/core";
       MatIconModule,
       MatButtonModule,
       RouterLink,
-      NgFor,
-      NgIf,
-      NgSwitch,
-      NgSwitchCase,
       TranslateModule,
    ],
 })
@@ -35,24 +30,21 @@ export class UserAppConfigComponent implements OnInit {
          await this.translateService.use("fr").toPromise();
 
          // Vérification que les traductions sont chargées
-         const translation = await this.translateService
-            .get("USER_CONFIG_APP.TITLE")
-            .toPromise();
+         const translation = await this.translateService.get(
+            "USER_CONFIG_APP.TITLE"
+         );
+
          console.log("Translation loaded:", translation);
       } catch (error) {
          console.error("Error loading translations:", error);
       }
    }
 
-   get themes() {
-      return this.themeService.getThemes();
+   isThemeSelected(themeName: ThemeType): boolean {
+      return this.themeService.selectedTheme()?.name === themeName;
    }
 
-   get selectedTheme() {
-      return this.themeService.selectedTheme();
-   }
-
-   setTheme(themeName: ThemeType) {
+   setTheme(themeName: ThemeType): void {
       this.themeService.setTheme(themeName);
    }
 }
