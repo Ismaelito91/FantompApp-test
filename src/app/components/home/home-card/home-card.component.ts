28f2aa86e46decedb35e2098a034742050b45e9a
Yohan Donjo <y.donjo@actongroup.com>
2025-07-18 16:15:44 +0200

    css animation of ghost winks

--- src/app/components/home/home-card/home-card.component.ts
+++ src/app/components/home/home-card/home-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, OnDestroy, AfterViewInit, inject } from "@angular/core";
+import { Component, OnInit, inject } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { Router } from "@angular/router";
 import { PageComponentService } from "../../../service/page-component.service";
@@ -10,10 +10,8 @@ import { PageComponentService } from "../../../service/page-component.service";
    templateUrl: "./home-card.component.html",
    styleUrls: ["./home-card.component.scss"],
 })
-export class HomeCardComponent implements OnInit, OnDestroy, AfterViewInit {
+export class HomeCardComponent implements OnInit {
    private pageComponentService = inject(PageComponentService);
-   showWink = false;
-   private animationTimeouts: number[] = [];
    private homePageLinkIds: Map<string, number> = new Map();
 
    constructor(private router: Router) {}
@@ -25,50 +23,6 @@ export class HomeCardComponent implements OnInit, OnDestroy, AfterViewInit {
             this.homePageLinkIds = new Map(Object.entries(data));
          }
       });
-   }
-
-   ngAfterViewInit(): void {
-      // Démarre l'animation après que la vue soit initialisée
-      setTimeout(() => {
-         this.startGhostAnimation();
-      }, 100);
-   }
-
-   ngOnDestroy(): void {
-      // Nettoie tous les timeouts quand on quitte la page
-      this.animationTimeouts.forEach((timeoutId) => clearTimeout(timeoutId));
-      this.animationTimeouts = [];
-   }
-
-   startGhostAnimation(): void {
-      // Nettoie les timeouts existants
-      this.animationTimeouts.forEach((timeoutId) => clearTimeout(timeoutId));
-      this.animationTimeouts = [];
-
-      // Animation en boucle : normal -> wink -> normal
-      const animateWink = () => {
-         // Commence par l'état normal
-         this.showWink = false;
-
-         // Après 3 secondes, fait un clin d'œil
-         const winkTimeout = setTimeout(() => {
-            this.showWink = true;
-
-            // Le clin d'œil dure 1 seconde
-            const normalTimeout = setTimeout(() => {
-               this.showWink = false;
-
-               // Recommence l'animation après 2 secondes
-               const restartTimeout = setTimeout(animateWink, 2000);
-               this.animationTimeouts.push(restartTimeout);
-            }, 1000);
-            this.animationTimeouts.push(normalTimeout);
-         }, 3000);
-         this.animationTimeouts.push(winkTimeout);
-      };
-
-      // Démarre l'animation
-      animateWink();
    }
 
    navigateToTools(): void {
