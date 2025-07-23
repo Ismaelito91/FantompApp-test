72e0db9d8bc2bd210b5b08c2db9147ab8bbc18dc
Mohamed TEBANI <m.tebani@actongroup.com>
2025-07-23 13:53:38 +0200

    feat(tools): improve visibility page

--- src/app/components/tools/visibility-check/visibility-check.component.ts
+++ src/app/components/tools/visibility-check/visibility-check.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { ButtonBackComponent } from "../../design-system/button-back/button-back.component";
+import { MatRadioButton } from "@angular/material/radio";
 
 @Component({
   selector: 'app-visibility-check',
-  imports: [ButtonBackComponent],
+  imports: [ButtonBackComponent, MatRadioButton],
   templateUrl: './visibility-check.component.html',
   styleUrl: './visibility-check.component.scss'
 })
