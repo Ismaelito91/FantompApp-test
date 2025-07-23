d5e1eca485fcd321e6937cf8e2860848954ed07d
Mohamed TEBANI <m.tebani@actongroup.com>
2025-07-23 15:15:55 +0200

    feat(tools): add next btn

--- src/app/components/tools/visibility-check/visibility-check.component.ts
+++ src/app/components/tools/visibility-check/visibility-check.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { ButtonBackComponent } from "../../design-system/button-back/button-back.component";
 import { MatRadioButton } from "@angular/material/radio";
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
-  selector: 'app-visibility-check',
-  imports: [ButtonBackComponent, MatRadioButton],
-  templateUrl: './visibility-check.component.html',
-  styleUrl: './visibility-check.component.scss'
+   selector: 'app-visibility-check',
+   imports: [ButtonBackComponent, MatRadioButton, MatButtonModule],
+   templateUrl: './visibility-check.component.html',
+   styleUrl: './visibility-check.component.scss'
 })
 export class VisibilityCheckComponent {
 
