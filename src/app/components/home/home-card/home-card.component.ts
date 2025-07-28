992311107b809ab0cd5cabb0ad7c5932502f883e
Ismael CEREZO <i.cerezo@actongroup.com>
2025-07-28 13:46:07 +0200

    refactor: réorganisation et harmonisation des cartes dans le composant...

--- src/app/components/home/home-card/home-card.component.ts
+++ src/app/components/home/home-card/home-card.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit, computed, inject, effect } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { Router } from "@angular/router";
+import { Router, RouterModule } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
 import { GhostAnimationService } from "../../../service/ghost-animation.service";
 import { PreloadService } from "../../../service/preload.service";
 
 @Component({
    selector: "app-home-card",
    standalone: true,
-   imports: [CommonModule],
+   imports: [CommonModule, RouterModule, MatIconModule],
    templateUrl: "./home-card.component.html",
    styleUrls: ["./home-card.component.scss"],
 })
@@ -15,29 +16,36 @@ export class HomeCardComponent implements OnInit {
    private ghostAnimationService = inject(GhostAnimationService);
    private preloadService = inject(PreloadService);
    private homePageLinkIds: Map<string, number> = new Map();
-   shouldPlayGhostAnimation = computed(() => this.ghostAnimationService.shouldPlayGhostAnimation());
+   shouldPlayGhostAnimation = computed(() =>
+      this.ghostAnimationService.shouldPlayGhostAnimation()
+   );
 
    constructor(private router: Router) {
       effect(() => {
          if (this.preloadService.isPreloaded()) {
             const preloadedData = this.preloadService.preloadedData();
-            if (preloadedData && Object.keys(preloadedData.homePageLinkIds).length > 0) {
-               this.homePageLinkIds = new Map(Object.entries(preloadedData.homePageLinkIds));
+            if (
+               preloadedData &&
+               Object.keys(preloadedData.homePageLinkIds).length > 0
+            ) {
+               this.homePageLinkIds = new Map(
+                  Object.entries(preloadedData.homePageLinkIds)
+               );
             }
          }
       });
    }
 
-   ngOnInit(): void {
-      
-   }
+   ngOnInit(): void {}
 
    navigateToTools(): void {
       this.router.navigate(["/tools"]);
    }
 
    navigateToHarassment(): void {
-      this.router.navigate(["/problems/" + this.homePageLinkIds.get("3.3_cyberharcelement") || ""]);
+      this.router.navigate([
+         "/problems/" + this.homePageLinkIds.get("3.3_cyberharcelement") || "",
+      ]);
    }
 
    onKeyDown(event: KeyboardEvent): void {
