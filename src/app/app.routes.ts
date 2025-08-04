7491051845a5426658125aee586ed9ac9bd56d05
Mohamed TEBANI <m.tebani@actongroup.com>
2025-08-04 14:37:43 +0200

    fix: home icons stroke

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -54,7 +54,7 @@ export const routes: Routes = [
             path: "blur-image",
             component: BlurImageComponent,
             pathMatch: "full",
-            data: { hideHeader: true, hideFooter: true }
+            data: { hideHeader: false, hideFooter: true }
          },
          {
             path: "visibility-check",
