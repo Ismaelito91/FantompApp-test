d1cb48182cbdef0a34eb8143fc8b4daf9380249b
Ismael Cerezo <i.cerezo@actongroup.com>
2025-09-04 10:07:48 +0200

    feat: supprimer l'animation d'entrée sur la page outils

--- src/app/components/tools/tools.component.ts
+++ src/app/components/tools/tools.component.ts
@@ -1,27 +1,26 @@
-import { animate, style, transition, trigger } from '@angular/animations';
-import { Component, inject } from '@angular/core';
-import { MatIconModule } from '@angular/material/icon';
-import { Router } from '@angular/router';
-import { TranslatePipe } from '@ngx-translate/core';
+import { animate, style, transition, trigger } from "@angular/animations";
+import { Component, inject } from "@angular/core";
+import { MatIconModule } from "@angular/material/icon";
+import { Router } from "@angular/router";
+import { TranslatePipe } from "@ngx-translate/core";
 
 @Component({
    selector: "app-tools",
    standalone: true,
    imports: [MatIconModule, TranslatePipe],
-   templateUrl: './tools.component.html',
-   styleUrl: './tools.component.scss',
+   templateUrl: "./tools.component.html",
+   styleUrl: "./tools.component.scss",
    animations: [
-      trigger('slideInOut', [
-         transition(':leave', [
-            style({ transform: 'translateX(0)' }),
-            animate('300ms ease-in-out', style({ transform: 'translateX(-100%)' }))
+      trigger("slideInOut", [
+         transition(":leave", [
+            style({ transform: "translateX(0)" }),
+            animate(
+               "300ms ease-in-out",
+               style({ transform: "translateX(-100%)" })
+            ),
          ]),
-         transition(':enter', [
-            style({ transform: 'translateX(-100%)' }),
-            animate('300ms ease-in-out', style({ transform: 'translateX(0)' }))
-         ])
-      ])
-   ]
+      ]),
+   ],
 })
 export class ToolsComponent {
    private readonly router = inject(Router);
@@ -31,7 +30,7 @@ export class ToolsComponent {
       this.isLeaving = true;
 
       setTimeout(() => {
-         this.router.navigate(['tools', url]);
+         this.router.navigate(["tools", url]);
       }, 300); // Même durée que le transition de leaveToLeft (300ms)
    }
 }
