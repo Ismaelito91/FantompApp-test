fdd0bc96a6be2613b2628651a320078e71495a31
Mohamed TEBANI <m.tebani@actongroup.com>
2025-09-19 11:45:27 +0200

    fix(card-4): fix switch-sad position

--- src/app/components/design-system/card-2/card-2.component.ts
+++ src/app/components/design-system/card-2/card-2.component.ts
@@ -4,10 +4,11 @@ import { ThemeService } from '../../../service/theme.service';
 import { MatIconModule } from '@angular/material/icon';
 import { SafeHtmlPipe } from '../../../pipes/safe-html.pipe';
 import { PageTranslationPipe } from "../../../pipes/page-translation.pipe";
+import { TranslatePipe } from '@ngx-translate/core';
 
 @Component({
    selector: 'app-card-2',
-   imports: [MatIconModule, SafeHtmlPipe, PageTranslationPipe],
+   imports: [MatIconModule, SafeHtmlPipe, PageTranslationPipe, TranslatePipe],
    templateUrl: './card-2.component.html',
    styleUrl: './card-2.component.scss'
 })
