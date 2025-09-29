840bc128eb5aafa2832cdf43cb8de472d84de5d1
Mohamed TEBANI <m.tebani@actongroup.com>
2025-09-29 17:19:17 +0200

    feat(template-message): add snackbar

--- src/app/components/design-system/template-message/template-message.component.ts
+++ src/app/components/design-system/template-message/template-message.component.ts
@@ -1,10 +1,11 @@
 import { Component, inject, input } from '@angular/core';
-import { TranslatePipe } from "@ngx-translate/core";
+import { TranslatePipe, TranslateService } from "@ngx-translate/core";
 import PageComponentModel from "../../../model/page-component.model";
 import { PageTranslationPipe } from "../../../pipes/page-translation.pipe";
 import { SafeHtmlPipe } from "../../../pipes/safe-html.pipe";
 import { ButtonComponent } from "../button/button.component";
 import { UtilsService } from '../../../service/utils.service';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
    selector: 'app-template-message',
@@ -20,12 +21,23 @@ import { UtilsService } from '../../../service/utils.service';
 export class TemplateMessageComponent {
    private readonly pageTranslationPipe = inject(PageTranslationPipe);
    private readonly utilsService = inject(UtilsService);
+   private readonly snackBar = inject(MatSnackBar);
+   private translateService = inject(TranslateService);
+
    data = input.required<PageComponentModel>();
 
    async copy() {
       try {
          if (navigator.clipboard && window.isSecureContext) {
             await navigator.clipboard.writeText(this.utilsService.htmlToTextViaElement(this.pageTranslationPipe.transform(this.data())?.description!));
+
+            // alert("Copié !");
+            this.snackBar.open(this.translateService.instant('COMMON.COPIED'), this.translateService.instant('COMMON.CLOSE'), {
+               duration: 1000, // durée en ms
+               horizontalPosition: 'center',
+               verticalPosition: 'bottom',
+               panelClass: 'snackbar-neutral'
+            });
          }
       } catch (err) {
          console.error("Erreur lors de la copie:", err);
