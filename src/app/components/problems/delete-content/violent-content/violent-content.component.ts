19217382526fe3022096629c07a543589dc4f76d
Mohamed TEBANI <m.tebani@actongroup.com>
2025-11-20 12:05:12 +0100

    feat: add  no-access dynamic page

--- src/app/components/problems/delete-content/violent-content/violent-content.component.ts
+++ src/app/components/problems/delete-content/violent-content/violent-content.component.ts
@@ -2,19 +2,17 @@ import { Component, inject, OnInit, signal } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
 import { ComponentStatus } from "../../../../model/enum/component-status.enum";
 import { ComponentType } from "../../../../model/enum/component-type.enum";
-import { Device } from "../../../../model/enum/device.enum";
 import PageComponentModel from "../../../../model/page-component.model";
+import { PageTranslationPipe } from "../../../../pipes/page-translation.pipe";
+import { SafeHtmlPipe } from "../../../../pipes/safe-html.pipe";
+import { PageComponentUtilsService } from "../../../../service/page-component-utils.service";
+import { PageComponentService } from "../../../../service/page-component.service";
 import { UtilsService } from "../../../../service/utils.service";
 import { ButtonBackComponent } from "../../../design-system/button-back/button-back.component";
 import { ButtonComponent } from "../../../design-system/button/button.component";
 import { Card4Component } from "../../../design-system/card-4/card-4.component";
 import { Card5Component } from "../../../design-system/card-5/card-5.component";
 import { DividerComponent } from "../../../design-system/divider/divider.component";
-import { LanguageService } from "../../../../service/language.service";
-import { PageComponentUtilsService } from "../../../../service/page-component-utils.service";
-import { PageComponentService } from "../../../../service/page-component.service";
-import { PageTranslationPipe } from "../../../../pipes/page-translation.pipe";
-import { SafeHtmlPipe } from "../../../../pipes/safe-html.pipe";
 
 const PAGE_CODE = "3.1.11/18_contenu_violent";
 
@@ -35,7 +33,6 @@ const PAGE_CODE = "3.1.11/18_contenu_violent";
 })
 export class ViolentContentComponent implements OnInit {
    readonly utilsService = inject(UtilsService);
-   private readonly languageService = inject(LanguageService);
    private readonly pageComponentUtils = inject(PageComponentUtilsService);
    private readonly pageComponentService = inject(PageComponentService);
    page = signal<PageComponentModel | null>({
@@ -47,116 +44,6 @@ export class ViolentContentComponent implements OnInit {
    ComponentStatus = ComponentStatus;
    reported: boolean = false;
 
-   card_4: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_4,
-      status: ComponentStatus.PUBLISHED,
-      position: 1,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P1.TITLE",
-            description:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P1.DESCRIPTION",
-         },
-      ],
-   };
-
-   card_5_1: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_5,
-      status: ComponentStatus.PUBLISHED,
-      position: 2,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P2.TITLE",
-            description:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P2.DESCRIPTION",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            position: 1,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P2.LINK.TITLE",
-                  description:
-                     "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P2.LINK.DESCRIPTION",
-                  staticImage: "assets/images/3018.png",
-               },
-            ],
-         },
-         {
-            id: 0,
-            type: ComponentType.TILE_CALL,
-            status: ComponentStatus.PUBLISHED,
-            position: 2,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P2.CALL.TITLE",
-                  staticImage: "assets/images/3018.png",
-               },
-            ],
-         },
-      ],
-   };
-
-   card_5_2: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_5,
-      status: ComponentStatus.PUBLISHED,
-      position: 3,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P3.TITLE",
-            description:
-               "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P3.DESCRIPTION",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            position: 1,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P3.LINK.TITLE",
-                  description:
-                     "PROBLEMS.DELETE_CONTENT.RESULTS.VIOLENT_CONTENT.P3.LINK.DESCRIPTION",
-                  staticImage: "assets/images/mariane.png",
-               },
-            ],
-         },
-      ],
-   };
-
    ngOnInit(): void {
       const state = history.state as { reported: boolean };
       this.reported = state.reported;
@@ -165,18 +52,18 @@ export class ViolentContentComponent implements OnInit {
    }
 
    private loadRootPage(): void {
-      let pageResources = this.pageComponentUtils.getComponentByCode(PAGE_CODE);
-      this.page.set(pageResources);
-      if (!pageResources) {
+      let pageViolentContent = this.pageComponentUtils.getComponentByCode(PAGE_CODE);
+      this.page.set(pageViolentContent);
+      if (!pageViolentContent) {
          this.pageComponentService
-            .getRootPageComponentsBySectionId(3)
+            .getRootPageComponentsBySectionId(1)
             .subscribe({
                next: (data) => {
                   this.pageComponentUtils.updateComponentMap(data);
-                  pageResources = this.pageComponentUtils.getComponentByCode(PAGE_CODE);
-                  this.page.set(pageResources);
+                  pageViolentContent = this.pageComponentUtils.getComponentByCode(PAGE_CODE);
+                  this.page.set(pageViolentContent);
                },
-               error: (err) => console.error("Erreur lors du chargement de home", err),
+               error: (err) => console.error("Erreur lors du chargement de problèmes", err),
             });
       }
    }
--- src/app/components/problems/hacking/no-access/no-access.component.ts
+++ src/app/components/problems/hacking/no-access/no-access.component.ts
@@ -1,236 +1,72 @@
-import { Component, inject } from "@angular/core";
+import { Component, inject, signal } from "@angular/core";
 import { TranslatePipe } from "@ngx-translate/core";
 import { ComponentStatus } from "../../../../model/enum/component-status.enum";
 import { ComponentType } from "../../../../model/enum/component-type.enum";
-import { Device } from "../../../../model/enum/device.enum";
 import PageComponentModel from "../../../../model/page-component.model";
+import { PageTranslationPipe } from "../../../../pipes/page-translation.pipe";
+import { SafeHtmlPipe } from "../../../../pipes/safe-html.pipe";
+import { PageComponentUtilsService } from "../../../../service/page-component-utils.service";
+import { PageComponentService } from "../../../../service/page-component.service";
+import { UtilsService } from "../../../../service/utils.service";
 import { ButtonBackComponent } from "../../../design-system/button-back/button-back.component";
-import { Card15Component } from "../../../design-system/card-15/card-15.component";
 import { ButtonComponent } from "../../../design-system/button/button.component";
 import { Card13Component } from "../../../design-system/card-13/card-13.component";
-import { UtilsService } from "../../../../service/utils.service";
-import { LanguageService } from "../../../../service/language.service";
+import { Card15Component } from "../../../design-system/card-15/card-15.component";
+import { DividerComponent } from "../../../design-system/divider/divider.component";
+
+const PAGE_CODE = "3.2.17_non_acces_compte";
 
 @Component({
    selector: "app-no-access",
    imports: [
       ButtonBackComponent,
       TranslatePipe,
-      Card15Component,
       ButtonComponent,
       Card13Component,
+      Card15Component,
+      DividerComponent,
+      PageTranslationPipe,
+      SafeHtmlPipe,
    ],
    templateUrl: "./no-access.component.html",
    styleUrl: "./no-access.component.scss",
 })
 export class NoAccessComponent {
    readonly utilsService = inject(UtilsService);
-   private readonly languageService = inject(LanguageService);
-
-   card_13: PageComponentModel = {
+   private readonly pageComponentUtils = inject(PageComponentUtilsService);
+   private readonly pageComponentService = inject(PageComponentService);
+   page = signal<PageComponentModel | null>({
       id: 0,
-      type: ComponentType.CARD_13,
-      status: ComponentStatus.PUBLISHED,
-      code: "code",
-      position: 1,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle: "PROBLEMS.HACKING.RESULTS.NO_ACCESS.CONTACT.TITLE",
-            description:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.CONTACT.DESCRIPTION",
-         },
-      ],
-   };
+      translations: [],
+      childrenIdList: [],
+   });
+   ComponentType = ComponentType;
+   ComponentStatus = ComponentStatus;
 
-   card_15_1: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_15,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.INSTAGRAM.TITLE",
-            staticImage: "assets/images/instagram.png",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.INSTAGRAM.LINK.TITLE",
-                  description:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.INSTAGRAM.LINK.DESCRIPTION",
-                  staticImage: "assets/images/instagram-2.png",
+   ngOnInit(): void {
+      this.loadRootPage();
+   }
+
+   private loadRootPage(): void {
+      let pageNoAccess = this.pageComponentUtils.getComponentByCode(PAGE_CODE);
+      this.page.set(pageNoAccess);
+      if (!pageNoAccess) {
+         this.pageComponentService
+            .getRootPageComponentsBySectionId(1)
+            .subscribe({
+               next: (data) => {
+                  this.pageComponentUtils.updateComponentMap(data);
+                  pageNoAccess =
+                     this.pageComponentUtils.getComponentByCode(PAGE_CODE);
+                  this.page.set(pageNoAccess);
                },
-            ],
-         },
-      ],
-   };
-   card_15_2: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_15,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.SNAPCHAT.TITLE",
-            staticImage: "assets/images/snapchat.png",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.SNAPCHAT.LINK.TITLE",
-                  description:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.SNAPCHAT.LINK.DESCRIPTION",
-                  staticImage: "assets/images/snapchat.png",
-               },
-            ],
-         },
-      ],
-   };
+               error: (err) =>
+                  console.error("Erreur lors du chargement de problèmes", err),
+            });
+      }
+   }
 
-   card_15_3: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_15,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.TIKTOK.TITLE",
-            staticImage: "assets/images/tiktok.png",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.TIKTOK.LINK.TITLE",
-                  description:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.TIKTOK.LINK.DESCRIPTION",
-                  staticImage: "assets/images/tiktok.png",
-               },
-            ],
-         },
-      ],
-   };
-
-   card_15_4: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_15,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.X.TITLE",
-            staticImage: "assets/images/x.png",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.X.LINK.TITLE",
-                  description:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.X.LINK.DESCRIPTION",
-                  staticImage: "assets/images/x.png",
-               },
-            ],
-         },
-      ],
-   };
-
-   card_15_5: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_15,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle:
-               "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.FACEBOOK.TITLE",
-            staticImage: "assets/images/facebook.png",
-         },
-      ],
-      children: [
-         {
-            id: 0,
-            type: ComponentType.ENRICHED_LINK,
-            status: ComponentStatus.PUBLISHED,
-            translations: [
-               {
-                  id: 0,
-                  countryRegion: this.languageService.language(),
-                  devices: [Device.ANDROID, Device.IOS, Device.WEB],
-                  firstTitle:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.FACEBOOK.LINK.TITLE",
-                  description:
-                     "PROBLEMS.HACKING.RESULTS.NO_ACCESS.SOCIAL_MEDIA.FACEBOOK.LINK.DESCRIPTION",
-                  staticImage: "assets/images/facebook.png",
-               },
-            ],
-         },
-      ],
-   };
-
-   card_14: PageComponentModel = {
-      id: 0,
-      type: ComponentType.CARD_14,
-      status: ComponentStatus.PUBLISHED,
-      translations: [
-         {
-            id: 0,
-            countryRegion: this.languageService.language(),
-            devices: [Device.ANDROID, Device.IOS, Device.WEB],
-            firstTitle: "Contacts des réseaux sociaux",
-         },
-      ],
-      children: [],
-   };
+   get sortedChildren() {
+      return this.pageComponentUtils.getSortedChildren(this.page());
+   }
 }
