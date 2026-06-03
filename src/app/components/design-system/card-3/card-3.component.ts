6e8b78e4b70505ffaefb188b20d86cdffec7ee33
Ismael Cerezo <i.cerezo@actongroup.com>
2026-06-03 14:43:00 +0200

    fix(a11y): corriger la structure des titres et liens pour le critère RGAA 8.1

--- src/app/components/design-system/card-3/card-3.component.ts
+++ src/app/components/design-system/card-3/card-3.component.ts
@@ -4,14 +4,15 @@ import { SafeHtmlPipe } from "../../../pipes/safe-html.pipe";
 import { PageTranslationPipe } from "../../../pipes/page-translation.pipe";
 import { TranslatePipe } from '@ngx-translate/core';
 import { ReplaceStringDarkPipe } from '../../../pipes/replace-string-dark.pipe';
+import { RouterLink } from '@angular/router';
 
 @Component({
    selector: 'app-card-3',
-   imports: [SafeHtmlPipe, PageTranslationPipe, TranslatePipe, ReplaceStringDarkPipe],
+   imports: [SafeHtmlPipe, PageTranslationPipe, TranslatePipe, ReplaceStringDarkPipe, RouterLink],
    templateUrl: './card-3.component.html',
    styleUrl: './card-3.component.scss'
 })
 export class Card3Component {
    data = input.required<PageComponentModel>();
-
+   link = input<string | null>(null);
 }
